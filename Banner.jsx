23a0d3c4166cbaba4dd9d63941ffb4efbecb8765
Gaurang Kalariya <81245476+gaurang721@users.noreply.github.com>
2023-01-29 19:41:08 +0530

    Update Banner.jsx

--- Banner.jsx
+++ Banner.jsx
@@ -1,1 +1,14 @@
+const Banner = (props) => {
+  return (
+    <>
+      <div className="common_banner">
+        <div className="BannerTitle">
+          <h1 className="BannerTitle">{props.title}</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Banner;
 
